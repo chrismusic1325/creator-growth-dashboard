de61905a01c350bdc5eb82df119a95f00c017546
chrismusic1325 <chrismusic1325@gmail.com>
2026-07-06 22:28:57 -0400

    Create delete platform button

---
+++ creator-growth-dashboard/src/components/platforms/DeletePlatformButton.jsx
@@ -0,0 +1,9 @@
+function DeletePlatformButton({ onDelete }) {
+  return (
+    <button type="button" onClick={onDelete}>
+      Delete
+    </button>
+  );
+}
+
+export default DeletePlatformButton;
