22d994a71f81fd664ab5ef5bae0241c4420c8c70
chrismusic1325 <chrismusic1325@gmail.com>
2026-07-07 20:57:38 -0400

    Create navigation bar

---
+++ creator-growth-dashboard/src/components/layout/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+
+function NavBar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default NavBar;
